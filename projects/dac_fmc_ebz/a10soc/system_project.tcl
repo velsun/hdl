b954230ce0065cceccf2ab2b419fb404ad44adf9
Istvan Csomortani <istvan.csomortani@analog.com>
2020-03-17 10:00:20 +0000

    dac_fmc_ebz/a10soc: Use balanced optimization mode
    Always a good idea to start from default, and change optimization mode
    of the tool if it's strict necessary.

--- projects/dac_fmc_ebz/a10soc/system_project.tcl
+++ projects/dac_fmc_ebz/a10soc/system_project.tcl
@@ -41,7 +41,7 @@ adi_project dac_fmc_ebz_a10soc [list \
   DEVICE    $device \
 ]
 
-set_global_assignment -name OPTIMIZATION_MODE "HIGH PERFORMANCE EFFORT"
+set_global_assignment -name OPTIMIZATION_MODE "BALANCED"
 
 source $ad_hdl_dir/projects/common/a10soc/a10soc_system_assign.tcl
 source $ad_hdl_dir/projects/common/a10soc/a10soc_plddr4_assign.tcl
